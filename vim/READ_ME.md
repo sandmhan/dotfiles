aafaf6217505ec0ca8121e5e45b8beec19866e90
sandmhan <austinsanders0105@gmail.com>
2023-12-16 00:51:49 -0500

    added root init.lua linkup

---
+++ vim/READ_ME.md
@@ -0,0 +1,3 @@
+# NVIM Config notes
+
+[~/.config/nvim/init.lua] contains the base lua script that runs the plugin scripts and vimrc file
--- vim/linkup.sh
+++ vim/linkup.sh
@@ -4,4 +4,4 @@ echo 'Linking up vimrc'
 ln -s ~/dotfiles/vim/vimrc ~/.vim/vimrc
 ln -s ~/dotfiles/vim/vimrc ~/.config/nvim/vimrc.vim
 ln -s ~/dotfiles/vim/init.lua ~/.config/nvim/lua/init.lua
-
+ln -s ~/dotfiles/vim/init_root.lua ~/.config/nvim/init.lua
