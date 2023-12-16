5a129718695841dffc27e0f181e5da0931c20a7e
sandmhan <austinsanders0105@gmail.com>
2023-12-16 00:56:12 -0500

    added nvim unlink commands

--- vim/cleanup.sh
+++ vim/cleanup.sh
@@ -2,4 +2,8 @@
 
 echo 'Unlinking vimrc'
 unlink ~/.vim/vimrc
-
+unlink ~/.config/nvim/vimrc.vim
+echo 'Unlinking root init.lua'
+unlink ~/.config/nvim/lua/init.lua
+echo 'Unlinking plugins init.lua'
+unlink ~/.config/nvim/init.lua
