31cbbbfd4bb6e584e1b9a2e939b5603ef19b6e07
sandmhan <austinsanders0105@gmail.com>
2025-03-24 23:46:54 -0400

    feat: moonlight added for remote desktop support. Still needs configuring to enable hardware acceleration

--- nix/desk_workstation/configuration.nix
+++ nix/desk_workstation/configuration.nix
@@ -49,11 +49,14 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+  };
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
+
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -100,6 +103,7 @@
 	htop-vim
     	tmux
 	pavucontrol
+	moonlight-qt
     #  thunderbird
     ];
   };
