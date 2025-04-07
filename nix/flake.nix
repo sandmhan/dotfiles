a7f690c6587c7d182384e951a69123122d12f79b
sandmhan <austinsanders0105@gmail.com>
2025-04-06 20:01:47 -0400

    feat(styles): added themes from librephoenix nixos-config

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My First Flake";
+  description = "Sandmhan's Flake";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
@@ -15,16 +15,31 @@
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = import nixpkgs {inherit system;};
+      # ---- SYSTEM SETTINGS ---- #
+      systemSettings = {
+        system = "x86_64-linux"; # system arch
+        hostname = "gaia"; # hostname
+        profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/Chicago"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
+        bootMode = "uefi"; # uefi or bios
+        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+        gpuType = "amd"; # amd, intel or nvidia; only makes some slight mods for amd at the moment
+      };
+
 
       # ----- USER SETTINGS ----- #
       userSettings = {
         username = "sandmhan";
+	theme = ""; 
+	wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
       };
     in {
 
       # System Configuration Output
       nixosConfigurations = {
-        nixos = lib.nixosSystem {
+        gaia = lib.nixosSystem {
           system = "x86_64-linux";
           modules = [./configuration.nix];
         };
