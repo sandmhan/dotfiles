d83d643c824546b04bc12ee25a01bf1e924a3ea0
sandmhan <austinsanders0105@gmail.com>
2025-11-10 22:46:14 -0500

    feat: more sway configurations

--- nix/homeModules/wm.nix
+++ nix/homeModules/wm.nix
@@ -1,6 +1,7 @@
 # based off https://git.sr.ht/~hervyqa/swayhome/tree/HEAD/item/home/wayland/sway.nix
 {
   pkgs,
+  lib,
   ...
 }:
 let
@@ -9,21 +10,30 @@ let
   down = "j";
   up = "k";
   right = "l";
-  modifier = "Control";
+  modifier = "Mod1";
 
-in {
+in
+{
   home = {
-    packages =
-      with pkgs;
-      [
-        rofi
-      ];
-    };
+    packages = with pkgs; [
+      rofi
+      waybar
+      autotiling
+    ];
+  };
 
   wayland.windowManager.sway = {
     enable = true;
+    checkConfig = false;
     config = {
       inherit modifier;
+      inherit left;
+      inherit down;
+      inherit up;
+      inherit right;
+
+      bars = [ ];
+
       focus = {
         followMouse = false;
         newWindow = "focus";
@@ -47,22 +57,88 @@ in {
       keybindings = {
         "${modifier}+T" = "exec ${pkgs.alacritty}/bin/alacritty";
         "${modifier}+C" = "kill";
-        "${modifier}+R" = "exec ${pkgs.rofi}/bin/rofi -show combi -modes combi -combi-modes 'window,drun,run' ";
+        "${modifier}+R" =
+          "exec ${pkgs.rofi}/bin/rofi -show combi -modes combi -combi-modes 'window,drun,run' ";
+        "${modifier}+B" = "exec pkill -SIGUSR1 waybar"; # only works if waybar has already been started
+        "Ctrl+Mod1+M" = "mode move";
+        "Ctrl+Mod1+R" = "mode resize";
+        "${modifier}+Shift+Backslash" = "layout toggle split";
+        "${modifier}+Shift+G" = "layout toggle splitv tabbed";
       }
-      //
-      builtins.listToAttrs (
-        builtins.concatMap (workspace: [
-          {
-            name = "${modifier}+${toString workspace}";
-            value = "workspace number ${toString workspace}";
-          }
-          {
-            name = "${modifier}+Shift+${toString workspace}";
-            value = "move container to workspace number ${toString workspace}; workspace ${toString workspace}";
-          }
-        ]) [1 2 3 4 5 6 7 8 9]
+      // builtins.listToAttrs (
+        builtins.concatMap
+          (workspace: [
+            {
+              name = "${modifier}+${toString workspace}";
+              value = "workspace number ${toString workspace}";
+            }
+            {
+              name = "${modifier}+Shift+${toString workspace}";
+              value = "move container to workspace number ${toString workspace}; workspace ${toString workspace}";
+            }
+          ])
+          [
+            1
+            2
+            3
+            4
+            5
+            6
+            7
+            8
+            9
+          ]
       );
+
+      modes = {
+        move = {
+          Comma = "move container to workspace prev; workspace prev";
+          Escape = "mode default";
+          Period = "move container to workspace next; workspace next";
+          S = "move scratchpad";
+        }
+        // builtins.listToAttrs (
+          builtins.concatMap
+            (workspace: [
+              {
+                name = toString workspace;
+                value = "move container to workspace number ${toString workspace}; workspace ${toString workspace}";
+              }
+            ])
+            [
+              1
+              2
+              3
+              4
+              5
+              6
+              7
+              8
+              9
+            ]
+        ); # improve to add directional focus and move hotkeys
+
+        resize = {
+          Escape = "mode default";
+          left = "resize shrink width 10 px";
+          down = "resize grow height 10 px";
+          up = "resize shrink height 10 px";
+          right = "resize grow width 10 px";
+        };
+      };
+
+      startup = [
+        { command = lib.getExe pkgs.autotiling; }
+        { command = lib.getExe pkgs.waybar; }
+      ];
+
+      window = {
+        titlebar = false;
+      };
+
     };
+
+    extraConfig = '''';
+
   };
 }
-
