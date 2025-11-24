d4af7dcf8ca13801a2768f271c0c2f72d2f2217e
sandmhan <austinsanders0105@gmail.com>
2025-11-23 22:45:56 -0500

    feat: added mini-files as in-editor file navigation

--- nix/homeModules/nvim.nix
+++ nix/homeModules/nvim.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   ...
 }:
 {
@@ -86,6 +87,16 @@
           desc = "Escape and Clear hlsearch";
         };
       }
+      {
+        mode = [
+          "n"
+        ];
+        key = "<leader>e";
+        action = "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>";
+        options = {
+          desc = "Mini-File: Open directory of current file";
+        };
+      }
     ];
 
     # Plugins
@@ -110,6 +121,47 @@
 
       # Show off available keymaps
       which-key.enable = true;
+
+      # File explorer
+      mini-files = {
+        enable = true;
+
+        settings = {
+          content = {
+            #filter = lib.nixvim.mkRaw "nil";
+            #highlight = lib.nixvim.mkRaw "nil";
+            #prefix = lib.nixvim.mkRaw "nil";
+            #sort = lib.nixvim.mkRaw "nil";
+          };
+          mappings = {
+            close = "q";
+            go_in = "l";
+            go_in_plus = "L";
+            go_out = "h";
+            go_out_plus = "H";
+            mark_goto = "'";
+            mark_set = "m";
+            reset = "<BS>";
+            reveal_cwd = "@";
+            show_help = "g?";
+            synchronize = "=";
+            trim_left = "<";
+            trim_right = ">";
+          };
+          options = {
+            permanent_delete = true;
+            use_as_default_explorer = true;
+          };
+          windows = {
+            #max_number = lib.nixvim.mkRaw "math.huge";
+            preview = true;
+            width_focus = 50;
+            width_nofocus = 15;
+            width_preview = 25;
+          };
+        };
+
+      };
 
       lsp = {
         enable = true;
