9144aa8027374b5933a7cdddc37aaedbe01ad27b
sandmhan <austinsanders0105@gmail.com>
2026-02-02 18:45:39 -0500

    feat: working frigate with one camera

--- systemModules/frigate.nix
+++ systemModules/frigate.nix
@@ -4,5 +4,29 @@
   ...
 }:
 {
-  services.frigate.enable = true;
+  services.frigate = {
+    enable = true;
+    hostname = "localhost";
+
+    settings = {
+      mqtt.enabled = true;
+
+      record = {
+        enabled = true;
+        retain = {
+          days = 7;
+          mode = "all";
+        };
+      };
+
+      cameras."fishtank".ffmpeg.inputs = [
+        {
+          path = "rtsp://192.168.50.174:554/ch0_0.h264";
+          roles = ["audio" "detect" "record"];
+        }
+      ];
+
+    };
+  };
+
 }
